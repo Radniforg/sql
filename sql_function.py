ab78084b4a6bd41314e8ab0022f0342d10bc75e5
Ivan Alexandrov <radniforg@yandex.ru>
2020-06-20 19:17:30 +0300

    Initial commit

---
+++ sql_function.py
@@ -0,0 +1,16 @@
+def create_db(): # создает таблицы
+    pass
+
+def get_students(course_id): # возвращает студентов определенного курса
+    pass
+
+def add_students(course_id, students): # создает студентов и
+                                       # записывает их на курс
+    pass
+
+
+def add_student(student): # просто создает студента
+    pass
+
+def get_student(student_id):
+    pass
